2124ad1ad9c3755cc2fb85d6c6868fa28fd60c46
ivandoric <idoric1@gmail.com>
2016-02-01 20:40:33 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+	<h1>Welcome to Olympos theme.</h1>
+    <p>
+        You can delete this text in index.php
+    </p>
+
+    
+<?php get_footer(); ?>
---
+++ woocommerce.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+    <?php woocommerce_content(); ?>
+<?php get_footer(); ?>
